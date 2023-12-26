faa752822a0978f966603b1a2faec331f19b98c1
Britteany <miszbritteany@gmail.com>
2023-12-25 21:27:52 -0500

    Added out of bounds  to placeShip function

--- webpack-demo/src/gameboard.js
+++ webpack-demo/src/gameboard.js
@@ -7,13 +7,25 @@ class Gameboard {
   }
   //handle orientation, check that it's on the board, no overlapping
   //return true if you placed the ship successfully and false if you didn't
-  //
+
   placeShip = (ship, row, column) => {
     this.grid[row][column] = ship;
+    if (
+      [row] < 0 ||
+      [row] >= this.gridSize ||
+      [column] < 0 ||
+      [column] >= this.gridSize
+    ) {
+      return "invalid ship coordinates for placement";
+    }
+    if ([column] + ship.length > this.gridSize) {
+      return "invalid ship placement out of bounds";
+    }
+    if ([row] + ship.length > this.gridSize) {
+      return "invalid ship placement out of bounds";
+    }
   };
-  receiveAttack = () => {
-
-  };
+  receiveAttack = () => {};
 }
 
-export default Gameboard
+export default Gameboard;
