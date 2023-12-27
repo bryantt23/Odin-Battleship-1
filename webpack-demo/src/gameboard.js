f28497a255baa92a87d24f7b5b311982d0b7c82f
Britteany <miszbritteany@gmail.com>
2023-12-27 17:49:33 -0500

    ship rotation and toggling

--- webpack-demo/src/gameboard.js
+++ webpack-demo/src/gameboard.js
@@ -5,25 +5,78 @@ class Gameboard {
       .fill(null)
       .map(() => Array(this.gridSize).fill(null));
   }
-  //handle orientation, check that it's on the board, no overlapping
-  //return true if you placed the ship successfully and false if you didn't
-
-  placeShip = (ship, row, column) => {
-    this.grid[row][column] = ship;
+  placeShip = (ship, row, column, isVertical) => {
+    // Check for valid coordinates and bounds
     if (
-      [row] < 0 ||
-      [row] >= this.gridSize ||
-      [column] < 0 ||
-      [column] >= this.gridSize
+      row < 0 ||
+      row >= this.gridSize ||
+      column < 0 ||
+      column >= this.gridSize
     ) {
-      return "invalid ship coordinates for placement";
+      return false; // Invalid ship coordinates for placement
     }
-    if ([column] + ship.length > this.gridSize) {
-      return "invalid ship placement out of bounds";
+
+    if (isVertical) {
+      if (row + ship.length > this.gridSize) {
+        return false; // Invalid ship placement out of bounds
+      }
+
+      for (let i = row; i < row + ship.length; i++) {
+        if (this.grid[i][column] !== null) {
+          return false; // Ship overlap
+        }
+      }
+
+      for (let i = row; i < row + ship.length; i++) {
+        this.grid[i][column] = ship;
+      }
+    } else {
+      if (column + ship.length > this.gridSize) {
+        return false; // Invalid ship placement out of bounds
+      }
+
+      for (let i = column; i < column + ship.length; i++) {
+        if (this.grid[row][i] !== null) {
+          return false; // Ship overlap
+        }
+      }
+
+      for (let i = column; i < column + ship.length; i++) {
+        this.grid[row][i] = ship;
+      }
     }
-    if ([row] + ship.length > this.gridSize) {
-      return "invalid ship placement out of bounds";
+
+    return true; // Ship placed successfully
+  };
+
+  rotateShip = (ship) => {
+    // Check if the ship can be rotated without going out of bounds
+    const canRotate =
+      ship.orientation === "horizontal"
+        ? ship.length + ship.row <= this.gridSize
+        : ship.length + ship.column <= this.gridSize;
+
+    if (canRotate) {
+      // Remove the ship from its current position
+      for (let i = 0; i < ship.length; i++) {
+        if (ship.orientation === "horizontal") {
+          this.grid[ship.row][ship.column + i] = null;
+        } else {
+          this.grid[ship.row + i][ship.column] = null;
+        }
+      } // Toggle the ship's orientation and re-place it
+
+      ship.toggleOrientation();
+
+      return this.placeShip(
+        ship,
+        ship.row,
+        ship.column,
+        ship.orientation === "vertical"
+      );
     }
+
+    return false; // Ship cannot be rotated
   };
   receiveAttack = () => {};
 }
