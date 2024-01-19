bf51579d4e1127905c78e8cd984064244e68ecca
Britteany <miszbritteany@gmail.com>
2024-01-18 21:44:33 -0500

    added CSS loader and bundled files

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,21 +1,25 @@
 const path = require("path");
 
 module.exports = {
-  entry: './src/game.js',
+  entry: "./src/game.js", // Assuming your main JavaScript file is named 'index.js'
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "src"), // Output to a 'dist' directory (recommended)
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/, // Match JavaScript files
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
+      },
+      {
+        test: /\.css$/, // Match CSS files
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
