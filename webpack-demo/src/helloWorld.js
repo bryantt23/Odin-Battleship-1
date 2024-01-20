e60eadc8da13dd34fc8c64ff58ce12bb59c8a11c
Britteany <miszbritteany@gmail.com>
2024-01-20 00:39:08 -0500

    DOM can access JS classes

---
+++ webpack-demo/src/helloWorld.js
@@ -0,0 +1,5 @@
+export class HelloWorld {
+    hi() {
+        alert("hello")
+    }
+};
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,8 @@
+
+import { HelloWorld } from './helloWorld';
+
+start();
+function start() {
+	const object = new HelloWorld()
+	object.hi()
+}
